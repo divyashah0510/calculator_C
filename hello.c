badefcc567bf8f5edab05e632236855a4752b7f8
Divya <ds3636005@gmail.com>
2022-08-09 19:58:27 +0530

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Hello World!\n");
+    scanf("%d",&a);
+    printf("Enter an integer number\n");
+    printf("%d",a+a);
+}
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+//This file has been modified in hello branh
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ cout<<"Hello World\n";
+ return 0;
+}
