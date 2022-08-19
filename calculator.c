25f413240d099ab247d553dd811f9aa0580ebc37
Divya <ds3636005@gmail.com>
2022-08-19 22:15:02 +0530

    Bug fixes

--- calculator.c
+++ calculator.c
@@ -13,162 +13,175 @@ int multiply1();
 int multiply2();
 int multiply3();
 int multiply4();
-int division1();
-int division2();
-int division3();
-int division4();
+int divide1();
+int divide2();
+int divide3();
+int divide4();
 void power();
 void modulus();
 void squareroot();
 void factorial();
+
 /*Main function*/
 void main()
 {
     int j;
     while(j!=1)
-    {int choice;
-    printf("Enter your choice for following:\n");
-    printf("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Power\n6.modulus\n7.squareroot\n8.factorial");
-    printf("Feed in your choice:\n");
-    scanf("%d",&choice);
-    int decision;
-        
-    switch (choice)
     {
+        int choice;
+        printf("\nEnter your choice for following:\n");
+        printf("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Power\n6.modulus\n7.squareroot\n8.factorial\n");
+        printf("Feed in your choice:\n");
+        scanf("%d",&choice);
+        int decision;
         
-        case 1:
-        printf("Enter how many terms you want to add ?\n");
-        printf("1.Addition for two terms\n2.Addition for three terms\n3.Addition for four terms\n4.Addition for five terms\n");
-        scanf("%d",&decision);
+        switch (choice)
+        {
         
-             switch (decision)
-            {
-                case 1:
-                    add1();
-                    break;
+            case 1:
+            printf("Enter how many terms you want to add ?\n");
+            printf("1.Addition for two terms\n2.Addition for three terms\n3.Addition for four terms\n4.Addition for five terms\n");
+            scanf("%d",&decision);
         
-                case 2:
-                    add2();
-                    break;
+                switch (decision)
+                {
+                    case 1:
+                        add1();
+                        break;
         
-                case 3:
-                    add3();
-                    break;
+                    case 2:
+                        add2();
+                        break;
         
-                case 4:
-                    add4();
-                    break;
+                    case 3:
+                        add3();
+                        break;
         
-                    default:printf("Wrong choice choosen!");
-                    break;
-            }
-            break;
-        case 2:
-        printf("Enter how many terms you want to subtract ?\n");
-        printf("1.Subtraction for two terms\n2.Subtraction for three terms\n3.Subtraction for four terms\n4.Subtraction for five terms\n");
-        scanf("%d",&decision);
-        
-             switch (decision)
-            {
-                case 1:
-                    subtract1();
-                    break;
-        
-                case 2:
-                    subtract2();
-                    break;
-        
-                case 3:
-                    subtract3();
-                    break;
-        
-                case 4:
-                    subtract4();
-                    break;
+                    case 4:
+                        add4();
+                        break;
         
                     default:printf("Wrong choice choosen!");
                     break;
-            }
-            break;
-        case 3:
-        printf("Enter how many terms you want to multiply ?\n");
-        printf("1.Multiplication for two terms\n2.Multiplication for three terms\n3.Multiplication for four terms\n4.Multiplication for five terms\n");
-        scanf("%d",&decision);
+                }
+                printf("\n");
+                break;
+            case 2:
+            printf("Enter how many terms you want to subtract ?\n");
+            printf("1.Subtraction for two terms\n2.Subtraction for three terms\n3.Subtraction for four terms\n4.Subtraction for five terms\n");
+            scanf("%d",&decision);
         
-             switch (decision)
-            {
-                case 1:
-                    multiply1();
-                    break;
+                switch (decision)
+                {
+                    case 1:
+                        subtract1();
+                        break;
         
-                case 2:
-                    multiply2();
-                    break;
+                    case 2:
+                        subtract2();
+                        break;
         
-                case 3:
-                    multiply3();
-                    break;
+                    case 3:
+                        subtract3();
+                        break;
         
-                case 4:
-                    multiply4();
-                    break;
-        
-                    default:printf("Wrong choice choosen!");
-                    break;
-            }
-            break;
-        case 4:
-        printf("Enter how many terms you want to multiply ?\n");
-        printf("1.Division for two terms\n2.Division for three terms\n3.Division for four terms\n4.Division for five terms\n");
-        scanf("%d",&decision);
-        
-             switch (decision)
-            {
-                case 1:
-                    divide1();
-                    break;
-        
-                case 2:
-                    divide2();
-                    break;
-        
-                case 3:
-                    divide3();
-                    break;
-        
-                case 4:
-                    divide4();
-                    break;
+                    case 4:
+                        subtract4();
+                        break;
         
                     default:printf("Wrong choice choosen!");
                     break;
-            }
-            break;
-        case 5:
-        power();
-        break;
-        case 6:
-        modulus();
-        break;
-        case 7:
-        squareroot();
-        break;
-        case 8:
-        factorial();
-        break;
+                }
+                break;
+            case 3:
+                printf("Enter how many terms you want to multiply ?\n");
+                printf("1.Multiplication for two terms\n2.Multiplication for three terms\n3.Multiplication for four terms\n4.Multiplication for     five terms\n");
+                scanf("%d",&decision);
+        
+                switch (decision)
+                {
+                    case 1:
+                        multiply1();
+                        break;
+        
+                    case 2:
+                        multiply2();
+                        break;
+        
+                    case 3:
+                        multiply3();
+                        break;
+        
+                    case 4:
+                        multiply4();
+                        break;
+        
+                    default:
+                        printf("Wrong choice choosen!");
+                        break;
+                }
+                break;
+            case 4:
+                printf("Enter how many terms you want to multiply ?\n");
+                printf("1.Division for two terms\n2.Division for three terms\n3.Division for four terms\n4.Division for five terms\n");
+                scanf("%d",&decision);
+        
+                switch (decision)
+                {
+                    case 1:
+                        divide1();
+                        break;
+        
+                    case 2:
+                        divide2();
+                        break;
+        
+                    case 3:
+                        divide3();
+                    break;
+        
+                    case 4:
+                        divide4();
+                        break;
+        
+                    default:printf("Wrong choice choosen!");
+                    break;
+                }
+                break;
+            case 5:
+                power();
+                break;
+            case 6:
+                modulus();
+                break;
+            case 7:
+                squareroot();
+                break;
+            case 8:
+                factorial();
+                break;
     
-    default:printf("Applied logic have missing flaws!");
+            default:
+                printf("Applied logic have missing flaws!");
+    
     }
     
-    printf("\n\n\n\nDo you want to continue calculating?\n1.No\n2.Yes\n");
-    scanf("%d",&j);
+    goback:
+        printf("\n\n\n\nDo you want to continue calculating?\n1.No\n2.Yes\n");
+        scanf("%d",&j);
+
     if (j==1)
     {
         exit(1);
     }
-    else
+    else if (j==2)
     {
         continue;
+    }
+    else
+    {
+        printf("\nWrong Input!");
+        goto goback;
     }
     
     
@@ -204,8 +217,8 @@ int add3()
 int add4()
 {
     int add1,add2,add3,add4,add5,sum;
-    printf("Enter five numbers:");
-    scanf("%d\n%d\n%d\n%d\n%d\n",&add1,&add2,&add3,&add4,&add5);
+    printf("Enter five numbers:\n");
+    scanf("%d\n%d\n%d\n%d\n%d",&add1,&add2,&add3,&add4,&add5);
     sum=add1+add2+add3+add4+add5;
     printf("The sum is %d",sum);
     return sum;
