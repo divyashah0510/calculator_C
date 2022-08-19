5780706ce7a65cbe12e99dfb426af55bf9428a39
Divya <ds3636005@gmail.com>
2022-08-19 22:45:49 +0530

    Delete unwanted files

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int a;
-    printf("Hello World!\n");
-    scanf("%d",&a);
-    printf("Enter an integer number\n");
-    printf("%d",a+a);
-}
--- hello.cpp
+++
@@ -1,9 +0,0 @@
-//This file has been modified in hello branh
-#include<iostream>
-using namespace std;
-
-int main()
-{
- cout<<"Hello World\n";
- return 0;
-}
